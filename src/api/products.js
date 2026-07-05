3a22555fd2e4974d03989dc00e7640f26dab731d
nepamzy <agunobert15@gmail.com>
2026-07-05 17:09:36 +0100

    Add missing getProductVariants export

--- src/api/products.js
+++ src/api/products.js
@@ -9,3 +9,7 @@ export async function getProduct(id) {
   const { data } = await api.get(`/products/${id}`);
   return data.data;
 }
+export async function getProductVariants(productId) {
+  const { data } = await api.get(`/products/${productId}/variants`);
+  return data.data;
+}
